d1503e501802d4d1f39f299fb1edd99ae6dbe3af
Ivan Gagarinov <dzencot@gmail.com>
2021-07-21 11:58:26 +0500

    fix aws creds

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,8 +12,7 @@ terraform {
 
 provider "aws" {
   region                  = "eu-central-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  shared_credentials_file = "./.aws/creds"
 }
 
 provider "datadog" {
